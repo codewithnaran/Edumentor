fa9befbe60e69d4275c33851b836b7e973f3c526
Narayan Bhusal <contactwithnaran@gmail.com>
2025-02-02 19:43:31 +0545

    Create ChatBotIframe.jsx

---
+++ components/ChatBotIframe.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const ChatBotIframe = () => {
+  return (
+    <div style={{ width: "100%", height: "100%", minHeight: "700px" }}>
+      <iframe
+        src="https://www.chatbase.co/chatbot-iframe/F_cSIUMh4iIwcKfyeRmuZ"
+        width="100%"
+        style={{ height: "100%", minHeight: "700px", border: "none" }}
+        frameBorder="0"
+      ></iframe>
+    </div>
+  );
+};
+
+export default ChatBotIframe;
